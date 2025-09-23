0ab091f57f601857598f8a3b12d1316ea86bbd61
Vinicius Damas <viniciusdamas97@gmail.com>
2025-09-23 09:50:31 -0300

    Etapa 1 - Criação do banco e tabela

---
+++ README.md
@@ -0,0 +1,7 @@
+Projeto de uma biblioteca usando o sqlite3 no python
+
+bibliotecas usadas
+sqlite3
+
+baixar extensão 
+sqlite viewer
--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import sqlite3
 
+#Conectar ao banco de dados (ou criar se não existir)
+conexao = sqlite3.connect("biblioteca.db")
+
+#Cria um cursor para executar comandos SQL
+cursor = conexao.cursor()
+
+#Criar tabela (senão existir)
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS biblioteca(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    autor TEXT NOT NULL,
+    ano INTEGER,
+    disponivel TEXT
+    )
+               
+""")
+print("Tabela criada!")
